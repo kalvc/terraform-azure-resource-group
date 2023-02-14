62ed3206f643bd3dad42f65db4d2ca0dcab1a8b6
Vishal Kalal <vishal.rajasthan@gmail.com>
2023-02-14 09:30:08 -0600

    Example Directory Added.

--- Example/main.tf
+++ Example/main.tf
@@ -1,5 +1,5 @@
 module "rg" {
-  source = "github.com/kalvc/terraform-azure-resource-group.git?ref=v1.0"
+  source = "github.com/kalvc/terraform-azure-resource-group.git?ref=v1.0" // Please check the Github Repo for the latest tags.
 
   resource_group_name = "RG_Name" // Give the Resource Group Name Here i.e. "RG-Demo".
   location            = "eastus"  // Give the Azure Region.
